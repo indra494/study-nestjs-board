a11da9591ed45c9a9d8369f75eeae6e78ad0cf77
indra <indra494@gmail.com>
2023-10-16 01:49:41 +0900

    typeORM 추가.

--- README.md
+++ README.md
@@ -19,6 +19,29 @@
 - 3. provider란 : 종속성을 주입할수 있는 것(스프링에서의 의존성주입과 비슷)
 - 4. decoration : java에서의 annotation
 - 5. @Injectable은 서비스단에서 사용할 수 있으며 스프링에서의 @Service 하고 유사함. @Inectable의 사용된 모듈(서비스)는 애플리케이션 전체에서 사용가능함.
+- 6. 파이프 : data valiadation check, data transformation 처리를 함
+  - 파이프 레벨에 따른 사용 예)
+    - 핸들러 레벨의 파이프 : @UsePipes(pipe)
+    - 파라메터 레벨의 파이프 : @Body('title', ParameterPipe) title,
+    - 글로벌 레벨의 파이프 : main.ts -> app.useGlobalPipes(GlobalPipes);
+  - 파라메터 종류
+    - ValidationPipe
+    - ParseIntPipe
+    - ParseBoolPipe
+    - ParseArrayPipe
+    - ParseUUIDPipe
+    - DefaultValuePipe
 
 ## 설치라이브러리
 - 1. uuid모듈 : npm install uuid --save
+- 2. 파이프모듈 : npm install class-validator class-transformer --save
+- 3. typeOrm모듈 or Postgresql모듈 : npm install pg typeorm @nestjs/typeorm --save
+- 4. pstgresSQL : https://postgresapp.com/downloads.html
+
+
+## 참조
+- 1. 파이프 참고 url : https://github.com/typestack/class-validator#manual-validation
+- 2. typeorm 참고 url : https://docs.nestjs.com/techniques/database
+- 3. typeorm repository 참고 url : https://typeorm.delightful.studio/classes/_repository_repository_.repository.html
+
+
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,37 +1,71 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './boards.model';
+import { BoardStatus } from './boards.status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
     constructor (private boardsService: BoardsService){};
     
     @Get('/')
-    getAllBoard(): Board[] {
+    getAllBoard(): Promise<Board[]> {
         return this.boardsService.getAllBoards();
     }
 
     @Get('/:id')
-    getBoardById(@Param('id') id : string): Board {
+    getBoardById(@Param('id') id : number): Promise <Board> {
         return this.boardsService.getBoardById(id);
     }
 
-    @Post('')
-    createBoard(@Body() createBoardDto: CreateBoardDto){
+    @Post()
+    @UsePipes(ValidationPipe)
+    createBoard(@Body() createBoardDto: CreateBoardDto) : Promise<Board> {
         return this.boardsService.createBoard(createBoardDto);
-    }
+    }    
 
     @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void {
-        this.boardsService.deleteBoard(id);
+    deleteBoard(@Param('id', ParseIntPipe) id): Promise <void> {
+        return this.boardsService.deleteBoard(id);
     }
 
     @Patch('/:id/status')
     updateBoardStatus(
-            @Param('id') id: string,
-            @Body('status') status: BoardStatus) {
+            @Param('id',ParseIntPipe) id: number,
+            @Body('status', BoardStatusValidationPipe) status: BoardStatus) : Promise<Board> {
 
         return this.boardsService.updateBoardStatus(id, status);
-    }
+    }    
+
+    /* 메모리로 스터디한 부분 */
+    // @Get('/')
+    // getAllBoard(): Board[] {
+    //     return this.boardsService.getAllBoards();
+    // }
+
+    // @Get('/:id')
+    // getBoardById(@Param('id') id : string): Board {
+    //     return this.boardsService.getBoardById(id);
+    // }
+
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // createBoard(@Body() createBoardDto: CreateBoardDto){
+    //     return this.boardsService.createBoard(createBoardDto);
+    // }
+
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: string): void {
+    //     this.boardsService.deleteBoard(id);
+    // }
+
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //         @Param('id') id: string,
+    //         @Body('status', BoardStatusValidationPipe) status: BoardStatus) {
+
+    //     return this.boardsService.updateBoardStatus(id, status);
+    // }
+    
 }
